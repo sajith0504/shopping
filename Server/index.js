dced2b8e9e568cf1e55028c13aabbc44bd786013
sajith0504 <sajithys1881@gmail.com>
2025-02-06 15:15:05 +0530

    s

--- Server/index.js
+++ Server/index.js
@@ -94,6 +94,6 @@ app.post('/verify-otp', async (req, res) => {
 
 
 // Start server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server running on http://localhost:3000');
 });
